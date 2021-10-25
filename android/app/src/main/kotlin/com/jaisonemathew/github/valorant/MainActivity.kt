fedf864a84fad4ead9879c1176455abe45ea2f20
Jaisonemathew <90517885+Jaisonemathew@users.noreply.github.com>
2021-10-25 22:33:22 +0530

    commit

---
+++ android/app/src/main/kotlin/com/jaisonemathew/github/valorant/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jaisonemathew.github.valorant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+
+        body: Center(child: Image(
+          image: AssetImage('images/valo.webp'),
+        ),
+        ),
+      ),
+    ),
+  );
+}
